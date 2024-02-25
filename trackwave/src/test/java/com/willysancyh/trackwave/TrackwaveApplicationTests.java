86c0915f838431316d1c1caeebf05b8e3793095e
willysanych <>
2024-02-25 18:41:11 +0300

    1  - initial commit

---
+++ trackwave/src/test/java/com/willysancyh/trackwave/TrackwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willysancyh.trackwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
